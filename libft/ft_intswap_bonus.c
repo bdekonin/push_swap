cc2588c88bb5285ddc6631398b15172987696a3b
bdekonin <bdekonin@student.codam.nl>
2021-08-27 18:12:44 +0200

    latest

--- libft/ft_intswap_bonus.c
+++ libft/ft_intswap_bonus.c
@@ -6,15 +6,15 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/11 14:29:58 by bdekonin      #+#    #+#                 */
-/*   Updated: 2021/05/11 14:30:13 by bdekonin      ########   odam.nl         */
+/*   Updated: 2021/05/12 13:57:11 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-void    ft_intswap(int *x,int *y)
+void	ft_intswap(int *x, int *y)
 {
-    int t;
+	int	t;
 
-    t   = *x;
-    *x   = *y;
-    *y   =  t;
+	t = *x;
+	*x = *y;
+	*y = t;
 }
--- utils/issorted.c
+++ utils/issorted.c
@@ -6,7 +6,7 @@
 /*   By: bdekonin <bdekonin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/17 21:30:27 by bdekonin      #+#    #+#                 */
-/*   Updated: 2021/05/11 14:29:13 by bdekonin      ########   odam.nl         */
+/*   Updated: 2021/05/12 13:57:53 by bdekonin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	issorted(t_node *head)
 		if (ft_atoi(head->content) <= ft_atoi(head->next->content))
 			head = head->next;
 		else
-			return(0);
+			return (0);
 	}
-	return(1);
+	return (1);
 }
